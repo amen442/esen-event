e680b5beb22a55cc3d6192ba59881a8292d77b2d
amen442 <amenallah.ibrahmi@esen.tn>
2026-04-15 17:39:43 +0100

    add github pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/anniv/",
+  base: "/esen-event/",
   plugins: [react()],
   publicDir: 'assets',
   server: {
